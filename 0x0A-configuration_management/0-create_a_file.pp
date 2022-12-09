03282be70bca4c4b463cfd0b70933b16a8de8861
mekapeu <kahtchekmekapeu@gmail.com>
2022-12-09 15:37:36 -0800

    Create a file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,9 @@
-# Using Puppet to create a file in /tmp.
+# manifest to create file in tmp directory
 file { '/tmp/holberton':
+  ensure  => file,
+  path    => '/tmp/holberton',
   mode    => '0744',
-  owner   => www-data,
-  group   => www-data,
-  content => 'I love Puppet'
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
 }
--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,9 @@
-# Using Puppet to install puppet-lint.
-package { 'puppet-lint':
-  ensure   => '2.1.1',
-  provider => 'gem'
+# manifest to create file in tmp directory
+file { '/tmp/holberton':
+  ensure  => file,
+  path    => '/tmp/holberton',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
 }
